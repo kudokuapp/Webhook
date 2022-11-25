9cc7d5db5f010baf711e9c06225eaade3b41feac
Furqon Wilogo <fdwilogo@gmail.com>
2022-11-25 04:54:00 +0300

    feat: update session

--- kudos.session.sql
+++ kudos.session.sql
@@ -6,7 +6,7 @@ ADD parentid INT;
 UPDATE users_final SET subscribe = false WHERE email = 'fathurrazanq@gmail.com'
 
 -- @block
-UPDATE users_final SET invited = false WHERE id = 3
+UPDATE users_final SET invited = false WHERE id = 11
 
 -- @block
 UPDATE users_final SET subscribe = true WHERE id = 45
