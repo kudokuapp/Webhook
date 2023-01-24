bf95ca3e0efc5b71a78428c16d287bd9deaa8e42
Furqon Wilogo <furqon@kudoku.id>
2023-01-24 18:25:14 +0000

    Update mongo.ts

--- pages/api/webhook/typeform/invited/mongo.ts
+++ pages/api/webhook/typeform/invited/mongo.ts
@@ -25,6 +25,7 @@ export default async function handler(
       const query = {
         username: '',
         password: '',
+        pin: '',
         firstName: data.firstname,
         lastName: data.lastname,
         email: data.email,
